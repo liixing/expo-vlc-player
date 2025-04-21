6cda390e4e3b695b365607daaed239b54701dff8
lixing <249568024@qq.com>
2025-04-21 17:01:30 +0800

    优化VLC播放器视图，添加播放源管理和资源清理功能

--- ios/ExpoVlcPlayerView.swift
+++ ios/ExpoVlcPlayerView.swift
@@ -4,25 +4,30 @@ import WebKit
 // This view will be used as a native component. Make sure to inherit from `ExpoView`
 // to apply the proper styling (e.g. border radius and shadows).
 class ExpoVlcPlayerView: ExpoView {
-    private var playerViewController: VLCPlayerViewController!
+  private var playerViewController: VLCPlayerViewController!
 
-    @objc var source: URL = URL(string: "about:blank")! {
-      didSet {
-        playerViewController.playSource(url: source)
-      }
+  @objc var source: URL = URL(string: "about:blank")! {
+    didSet {
+      playerViewController.playSource(url: source)
     }
+  }
 
   required init(appContext: AppContext? = nil) {
     super.init(appContext: appContext)
-      playerViewController = VLCPlayerViewController()
-      playerViewController.view.frame = self.bounds
-      playerViewController.view.autoresizingMask = [.flexibleWidth, .flexibleHeight]
-      self.addSubview(playerViewController.view)
+    playerViewController = VLCPlayerViewController()
+    playerViewController.view.frame = self.bounds
+    playerViewController.view.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+    self.addSubview(playerViewController.view)
   }
 
-    override func layoutSubviews() {
-      super.layoutSubviews()
-      playerViewController.view.frame = self.bounds
-    }
+  override func layoutSubviews() {
+    super.layoutSubviews()
+    playerViewController.view.frame = self.bounds
+  }
+
+  deinit {
+    playerViewController.stopPlayback()
+  }
 }
+
 
--- ios/VLCPlayerViewController.swift
+++ ios/VLCPlayerViewController.swift
@@ -9,39 +9,39 @@ import UIKit
 import VLCKit
 import AVFoundation
 
-class VLCPlayerViewController: UIViewController, VLCMediaPlayerDelegate{
-  private var mediaPlayer: VLCMediaPlayer!
-    
+class VLCPlayerViewController: UIViewController, VLCMediaPlayerDelegate {
+  private var mediaPlayer: VLCMediaPlayer = VLCMediaPlayer()
+  private var currentURL: URL?
+
   override func viewDidLoad() {
     super.viewDidLoad()
+    setupPlayer()
+  }
+
+  private func setupPlayer() {
+    mediaPlayer.drawable = self.view
+    mediaPlayer.delegate = self
+    self.view.backgroundColor = .black
+    self.view.isUserInteractionEnabled = false
+
+    try? AVAudioSession.sharedInstance().setActive(false, options: .notifyOthersOnDeactivation)
   }
 
   func playSource(url: URL) {
-      if(mediaPlayer != nil){
-          cleanupPlayer()
-      }
+    guard url != currentURL else { return } // 如果是相同地址就不重复播放
 
-      mediaPlayer = VLCMediaPlayer()
-
-      mediaPlayer.drawable = self.view
-      mediaPlayer.delegate = self
-      mediaPlayer.media = VLCMedia.init(url: url)
-      
-      mediaPlayer.play()
-      
-
-      self.view.backgroundColor = .black
-      self.view.isUserInteractionEnabled=false
-
-    try? AVAudioSession.sharedInstance().setActive(false, options: AVAudioSession.SetActiveOptions.notifyOthersOnDeactivation)
+    currentURL = url
+    mediaPlayer.media = VLCMedia(url: url)
+    mediaPlayer.play()
   }
-    
-    func cleanupPlayer(){
-        if(mediaPlayer != nil){
-            mediaPlayer.pause()
-            mediaPlayer = nil
-            
-        }
-    }
-    
+
+  func stopPlayback() {
+    mediaPlayer.stop()
+    currentURL = nil
+  }
+
+  deinit {
+    mediaPlayer.stop()
+    mediaPlayer.delegate = nil
+  }
 }
